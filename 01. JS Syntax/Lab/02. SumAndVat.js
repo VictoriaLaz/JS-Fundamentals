0c1c86e8a64dc930eaad2df4861f2b9e100b3a9e
Victoria Lazarova <lazarova.victoria@gmail.com>
2017-05-24 13:05:24 +0300

    Added sum and VAT

--- 01. JS Syntax/Lab/02. SumAndVat.js
+++ 01. JS Syntax/Lab/02. SumAndVat.js
@@ -2,6 +2,13 @@
  * Created by Vicky on 5/23/2017.
  */
 
-function sumAndVAT([]) {
-    let sumAndVat = 
+function sumAndVAT(input) {
+    let sum = 0;
+    for (let i of input) {
+        sum += i;
+    }
+    console.log(`sum = ${sum}`);
+    console.log(`VAT = ${sum * 0.2}`);
+    console.log(`total = ${sum * 1.2}`);
 }
+
