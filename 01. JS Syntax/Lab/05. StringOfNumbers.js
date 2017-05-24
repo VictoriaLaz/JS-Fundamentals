ff1172c07aabb416b507c8f5af071ed09755c811
Victoria Lazarova <lazarova.victoria@gmail.com>
2017-05-24 14:34:57 +0300

    Added 05.String of numbers

---
+++ 01. JS Syntax/Lab/05. StringOfNumbers.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Vicky on 5/24/2017.
+ */
+function stringON(n) {
+    let string ='';
+    for(let i=1; i<=n; i++){
+        string += i;
+    }
+    console.log(string);
+}
+
+stringON('11');
